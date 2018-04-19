3a35ad6075dcaf569bded8cf8ddcce08e390b795
Mark Ammari <mark.ammari@timeinc.com>
2018-04-19 11:32:08 -0400

    created vue app version of fetching api

---
+++ awsapivuejs2/src/components/UserInput.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <button type="button" class="btn btn-primary" @click="checkSuccess">Click</button>
+        <input type="text" placeholder="Value" v-on:keydown.enter="changeUrl">
+        <br></br>
+        <p>{{ url }}</p>
+    </div>    
+</template>
+
+<script>
+    export default {
+        props: ['url'],
+    
+    methods: {
+        checkSuccess(){
+            console.log('I am running')
+            fetch(this.url)
+              .then(response => response.json())
+                .then(myJson => vm.successRate = myJson.success)
+          },
+        changeUrl() {
+            this.$emit('changeUrlbase', this.url)
+        }  
+    }
+}
+</script>
